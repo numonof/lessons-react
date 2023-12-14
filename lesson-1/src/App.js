24e448b86b573bee71327045a929dda78dc13464
numonof <islomxonnumonov3701@gmail.com>
2023-12-14 12:30:44 +0300

    Lesson one end

--- lesson-1/src/App.js
+++ lesson-1/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main className='bg-sky-950 min-h-screen'>
+      
+    <div className="  xl:container xl:mx-auto  bg-sky-950  ">
+      <header className="">
+         <h1 className='font-sans font-bold text-center text-5xl text-slate-200 '>Hello firs react page</h1>
+        <p className='font-sans text-white'>
+          React kutubxonalarini ishiga tushurish uchun Node.js kerak bo'ladi. 
+          Uni o'rnatganimizdan so'ng CMD buyruqlar oynasini ochib u yerda <b>"node -v"</b> buyrug'ini kiritsak bizga node.js ning versiyasini chiqarib berad.
+          Agar bizda Node.js ornatilgan bo'lsa uning ichida <b>" npx "</b> buyrug'i bo'ladi.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3 className='font-sans font-bold text-center text-3xl text-slate-200'>NPX</h3>
+        <p className='font-sans text-white'>
+          NPX - bizga react kutubxonalarini yasash uchun kerak bo'ladi.
+          React kutubxonalarini yaratsih uchun:
+          <b>"npx create-react-app project-one"</b>
+          buyrug'idan foydalanamiz
+        </p>
+        <h3 className='font-sans font-bold text-center text-3xl text-slate-200' >README.md</h3>
+        <p  className='font-sans text-white'>proect haqida umumiy malumotlarni o'zida saqlaydi. 
+        </p>
+        <h3 className='font-sans font-bold text-center text-3xl text-slate-200'>package.json</h3>
+        <p  className='font-sans text-white'>proect haqidagi malumotlarni o'zida saqlaydi. 
+        </p>
+
+        </header>
     </div>
+    </main>
   );
 }
 
